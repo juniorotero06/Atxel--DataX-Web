6a0198aaa54bb1532ecfac07eb2d005649264997
juniorotero06 <dgrotero697@gmail.com>
2022-04-12 16:50:25 -0500

    rol admin controller

---
+++ src/controllers/AdminRoleController.js
@@ -0,0 +1,7 @@
+const User = require("../../database/config/database-config").User;
+const License = require("../../database/config/database-config").License;
+const UserRolLicense =
+  require("../../database/config/database-config").UserRolLicense;
+const Joi = require("@hapi/joi");
+
+exports.adminRegisterToUser = async (req, res) => {};
