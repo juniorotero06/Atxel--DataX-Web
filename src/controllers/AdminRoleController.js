5eeea7b5716e9135b295aafd03229e7787686b51
juniorotero06 <dgrotero697@gmail.com>
2022-04-19 14:23:27 -0500

    ADMIN ROLE

--- src/controllers/AdminRoleController.js
+++ src/controllers/AdminRoleController.js
@@ -4,4 +4,22 @@ const UserRolLicense =
   require("../../database/config/database-config").UserRolLicense;
 const Joi = require("@hapi/joi");
 
-exports.adminRegisterToUser = async (req, res) => {};
+const schemaUserRegister = Joi.object({
+  name: Joi.string().required(),
+  lastname: Joi.string().required(),
+  phone: Joi.string().required(),
+  email: Joi.string().min(6).max(255).required().email(),
+  password: Joi.string().min(6).max(1024).required(),
+  licenseId: Joi.string().max(10),
+  rol: Joi.string().required(),
+});
+
+exports.adminRegisterToUser = async (req, res) => {
+  const { error } = schemaUserRegister.validate(req.body);
+  if (error) return res.status(400).json({ error: error.details[0].message });
+
+  const user = await User.findOne({
+    where: { email: req.body.email },
+  });
+  if (user) return res.status(400).json({ error: "Usuario ya registrado" });
+};
